7e4ffb6607a2ea777e9daca67deb6ddfc7992757
Ariel Feingold <feingold.ariel@gmail.com>
2018-01-03 23:46:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function forLoop(array){
   for(var i = 0; i < 25; i++)
     if([i] === 1){
@@ -7,10 +8,22 @@ function forLoop(array){
     }
     return array
   };
+=======
+// function forLoop(array){
+//   for(var i = 0; i < 25; i++)
+//     if([i] === 1){
+//       array.push(`I am 1 strange loop.`)
+//     }else{
+//       array.push(`I am ${i} strange loops.`)
+//     }
+//     return array
+//   };
+>>>>>>> 257732f86777ec0582f92165c53518e959ea50a1
 
 function whileLoop(n){
   while(n > 0){
       console.log(n)
+<<<<<<< HEAD
       n--
     }
     return "done"
@@ -26,3 +39,9 @@ function doWhileLoop(array){
   } while(array.length > 0 | maybeTrue() === true )
   return array
 }
+=======
+      n = n--
+    }
+    return "done"
+}
+>>>>>>> 257732f86777ec0582f92165c53518e959ea50a1
